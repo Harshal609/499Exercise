9997a1e6bf59d3022f5c3281d99805e3d0507409
Harshal609 <patelharshal321@gmail.com>
2022-09-16 21:47:46 -0700

    Starter code for palindrome done!

---
+++ Palindrome.py
@@ -0,0 +1,13 @@
+#Name:Harshal Patel
+#SID: 28994291
+#Date: Sept - 16 - 2022
+#Program: Palindrome
+#Description: Checks the input array for a palindrome, and returns a yes if there is one.
+
+words = ["Computer","science","is ","aswesome","we","love","it","here"]
+
+
+
+
+
+
